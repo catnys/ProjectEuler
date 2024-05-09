1c473791d9aa06fc9ed50325ff1369bfac6d33f0
aintcat <92661367+aintburak@users.noreply.github.com>
2024-05-09 09:57:25 +0300

    Create spiral_diagonals.h

---
+++ Problem-023/spiral_diagonals.h
@@ -0,0 +1,7 @@
+#ifndef SPIRAL_DIAGONALS_H
+#define SPIRAL_DIAGONALS_H
+
+// Function to calculate the sum of diagonals in an n x n spiral
+long long sumOfSpiralDiagonals(int n);
+
+#endif // SPIRAL_DIAGONALS_H
