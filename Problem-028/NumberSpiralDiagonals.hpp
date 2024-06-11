e2a42cb64e8c6a41cd767005a64cf47552335afa
I'm just a cat <92661367+catnys@users.noreply.github.com>
2024-06-11 09:16:10 +0300

    Problem-028: Create NumberSpiralDiagonals.hpp

---
+++ Problem-028/NumberSpiralDiagonals.hpp
@@ -0,0 +1,7 @@
+#ifndef NUMBERSPIRALDIAGONALS_HPP
+#define NUMBERSPIRALDIAGONALS_HPP
+
+// Function to calculate the sum of the numbers on the diagonals in an n x n spiral.
+long long sumOfDiagonals(int n);
+
+#endif
