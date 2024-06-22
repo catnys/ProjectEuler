743401763a8f420a8f5d011b6b004e0a510cd108
I'm just a cat <92661367+catnys@users.noreply.github.com>
2024-06-22 12:54:03 +0300

    Problem-058: Create spiralprimes.hpp

---
+++ Problem-058/spiralprimes.hpp
@@ -0,0 +1,7 @@
+#ifndef SPIRALPRIMES_HPP
+#define SPIRALPRIMES_HPP
+
+// Function to find the side length of the square spiral where the ratio of primes on the diagonals falls below 10%
+int findSideLength();
+
+#endif
