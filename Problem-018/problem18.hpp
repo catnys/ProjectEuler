a0c2bee3971c643bf7bb7129e0bbc6f7558e0279
I'm just a cat <92661367+catnys@users.noreply.github.com>
2024-06-23 15:25:50 +0300

    Create problem18.hpp for p18

---
+++ Problem-018/problem18.hpp
@@ -0,0 +1,11 @@
+#ifndef PROBLEM18_HPP
+#define PROBLEM18_HPP
+
+#include <vector>
+
+class Problem18Solver {
+public:
+    static int solveMaxPathSum(const std::vector<std::vector<int>>& triangle);
+};
+
+#endif // PROBLEM18_HPP
